430fca97fee968e5226e94ceb7e2f108efded495
greysonmitra <greyson.mitra@outlook.com>
2015-09-24 08:28:13 -0600

    Setup a Popup Project

---
+++ src/input/controller/InputRunner.java
@@ -0,0 +1,55 @@
+package input.controller;
+
+public class InputRunner
+{
+	public static void main (String [] args)
+	{
+		//Thing myThing = new Thing();
+		//myThing.Start();
+		PopupController myInput = new PopupController();
+		myInput.start();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
