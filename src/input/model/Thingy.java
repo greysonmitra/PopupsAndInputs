84e35f0c4304c6c703ad2b345840646b9f2f3439
greysonmitra <greyson.mitra@outlook.com>
2015-09-30 08:28:02 -0600

    Added some JavaDoc
     I put Javadoc for some constructors.

--- src/input/model/Thingy.java
+++ src/input/model/Thingy.java
@@ -10,12 +10,23 @@ public class Thingy
 	private int age;
 	private double weight;
 	
+	/**
+	 * Creates objects with bad values. There are impossible values meant to be reset.
+	 */
+	
 	public Thingy()
 	{
 		name = "";
 		age = -99;
 		weight = -.008;
 	}
+	
+	/**
+	 * Sets the ridiculous values to the objects. Assigns the left to the right.
+	 * @param name
+	 * @param age
+	 * @param weight
+	 */
 	
 	public Thingy(String name, int age, double weight)                       //CONSTRUCTOR
 	{
@@ -38,5 +49,24 @@ public class Thingy
 	public double getWeight()
 	{
 		return weight;
+	}
+	
+	
+	
+	
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
 	}
 }
